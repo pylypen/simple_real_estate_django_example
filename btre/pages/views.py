95eb6fc3a2c3a7d20721df4bd528c13a9bc87370
Denys Pylypenko <pylypen@gmail.com>
2021-01-22 21:18:36 +0200

    views for listings, realtors, index, about and search pages

--- btre/pages/views.py
+++ btre/pages/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
+from listings.choices import bedroom_choices, price_choices, state_choices
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    return render(request, 'pages/index.html', {
+        'listings': listings,
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
+        "state_choices": state_choices,
+    })
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date').all()[:3]
+    mvp_realtor = Realtor.objects.filter(is_mvp=True).first()
+
+    return render(request, 'pages/about.html', {
+        'realtors': realtors,
+        'mvp_realtor': mvp_realtor
+    })
