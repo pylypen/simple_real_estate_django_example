7b9259a8e7e019fb2fd3bf35ee140a9e217501b7
Denys Pylypenko <pylypen@gmail.com>
2021-01-23 17:25:41 +0200

    Views and logic for login, logout, register and dashboard. Addind authentication

---
+++ btre/accounts/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+
+
+def login(request):
+    if request.method == 'POST':
+        # Get form data
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authenticate
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Incorrect username or password')
+
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    if request.user.is_authenticated:
+        auth.logout(request)
+        messages.info(request, 'Logout done')
+
+    return redirect('login')
+
+
+def register(request):
+    if request.method == 'POST':
+        # Get form data
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['password2']
+
+        # Validation
+        if password != confirm_password:
+            messages.error(request, 'Password do not match')
+        elif User.objects.filter(username=username).exists():
+            messages.error(request, 'Username taken')
+        elif User.objects.filter(email=email).exists():
+            messages.error(request, 'Email taken')
+        else:
+            user = User.objects.create_user(
+                username=username,
+                password=password,
+                email=email,
+                first_name=first_name,
+                last_name=last_name
+            )
+            user.save()
+            messages.success(request, 'User created successfully!')
+
+            return redirect('login')
+
+    return render(request, 'accounts/register.html')
+
+
+def dashboard(request):
+    if request.user.is_authenticated:
+        return render(request, 'accounts/dashboard.html')
+
+    return redirect('login')
--- btre/app/urls.py
+++ btre/app/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('accounts/', include('accounts.urls')),
     path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
